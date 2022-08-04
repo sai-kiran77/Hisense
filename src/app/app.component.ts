7eff336abc11e75aaef033bc839c300efef33765
sai kiran <sai.kiran3916@gmail.com>
2022-08-04 16:05:09 +0530

    build pushed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, HostListener } from '@angular/core';
 
 @Component({
@@ -7,4 +8,16 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'hisense';
+  
+
+  constructor(private http: HttpClient) {
+    this.http.get('https://hisense-apis.bloginguru.com/v1/pages/metadata?pageName=home').subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeModule } from './pages/home/home.module';
 import { SwiperModule } from 'swiper/angular';
 import { ShellComponent } from './components/shell/shell.component';
 import { ComponentsModule } from './components/components.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ComponentsModule } from './components/components.module';
     AppRoutingModule,
     HomeModule,
     SwiperModule,
-    ComponentsModule
+    ComponentsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
