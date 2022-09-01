c8532092d1b447aef80f04c7671a9c5e9ee62395
sai kiran <sai.kiran3916@gmail.com>
2022-09-01 08:24:38 +0530

    build

--- dist/hisense/browser/main.js
+++ dist/hisense/browser/main.js
@@ -1243,7 +1243,7 @@ class MainSliderComponent {
                 mobile: `${src_environments_environment__WEBPACK_IMPORTED_MODULE_1__.environment.baseApiUrl}/media/pages/home/slider/slide-1-300822-mobile.webp`
             },
             {
-                url: ' /c/uhd-tv',
+                url: '/c/uhd-tv',
                 image: `${src_environments_environment__WEBPACK_IMPORTED_MODULE_1__.environment.baseApiUrl}/media/pages/home/slider/slide-2-300822.webp`,
                 mobile: `${src_environments_environment__WEBPACK_IMPORTED_MODULE_1__.environment.baseApiUrl}/media/pages/home/slider/slide-2-300822-mobile.webp`
             },
