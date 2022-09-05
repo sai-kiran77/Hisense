6d09a9b5d907db5379213dc8241dc92546c9b9ae
sai kiran <sai.kiran3916@gmail.com>
2022-09-05 18:50:43 +0530

    changes

--- src/app/components/main-slider/main-slider.component.ts
+++ src/app/components/main-slider/main-slider.component.ts
@@ -44,7 +44,6 @@ export class MainSliderComponent implements OnInit {
         setTimeout(()=>{
           let swiper = new Swiper(".mySwiper", {
             slidesPerView: 1,
-            spaceBetween: 30,
             loop: true,
             pagination: {
               el: ".swiper-pagination",
