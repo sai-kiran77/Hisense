04d08fa7e2bd4f33a85e469e35cf02566823f1dc
sai kiran <sai.kiran3916@gmail.com>
2022-09-07 17:03:05 +0530

    build of removed timer

--- dist/hisense/browser/src_app_pages_fifa-event_fifa-event_module_ts.js
+++ dist/hisense/browser/src_app_pages_fifa-event_fifa-event_module_ts.js
@@ -30,40 +30,6 @@ class FifaEventComponent {
         console.log('from fifa');
     }
     ngOnInit() {
-        // Set the date we're counting down to
-        let countDownDate = new Date("Sep 15, 2022 00:00:00").getTime();
-        // Update the count down every 1 second
-        this.x = setInterval(() => {
-            // Get today's date and time
-            let now = new Date().getTime();
-            // Find the distance between now and the count down date
-            let distance = countDownDate - now;
-            // Time calculations for days, hours, minutes and seconds
-            const days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
-            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
-            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            if (this.days !== days) {
-                console.log('days');
-                this.days = days;
-            }
-            if (this.hours !== hours) {
-                console.log('hours');
-                this.hours = hours;
-            }
-            if (this.minutes !== minutes) {
-                console.log('minutes');
-                this.minutes = minutes;
-            }
-            // Output the result in an element with id="demo"
-            // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-            //   + minutes + "m " + seconds + "s ";
-            // If the count down is over, write some text 
-            if (distance < 0) {
-                clearInterval(this.x);
-                // document.getElementById("demo").innerHTML = "EXPIRED";
-            }
-        }, 1000);
     }
     ngDoCheck() {
         console.log('chnage detected');
