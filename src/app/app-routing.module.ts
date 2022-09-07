0aeb803d0b30c00678c9752ded2eee0fde55121b
sai kiran <sai.kiran3916@gmail.com>
2022-09-07 15:37:06 +0530

    routed updated ti FIFA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
       //   loadChildren: () => import('./pages/delightful-india/delightful-india.module').then(m => m.DelightfulIndiaModule)
       // },
       {
-        path: 'fifa-2022',
+        path: 'FIFA-2022',
         loadChildren: () => import('./pages/fifa-event/fifa-event.module').then(m => m.FifaEventModule)
       },
       {
