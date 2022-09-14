278c464a051bb7587d23deeecdadfb1d62e86b25
sai kiran <sai.kiran3916@gmail.com>
2022-09-14 19:27:07 +0530

    improvements

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -10,15 +10,18 @@ import { GlobalStateService } from 'src/app/services/global-state.service';
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent implements OnInit,OnDestroy {
+export class CategoryComponent implements OnInit, OnDestroy {
 
   metaData: any;
   alive = true;
   show404 = false;
   // fragment: any = '';
-  fallbackImg = 'https://www.hisense-india.com/wp-content/themes/hisense/images/tv/A6H.webp'
+  fallbackImg = 'https://www.hisense-india.com/wp-content/themes/hisense/images/tv/A6H.webp';
+  purchaseLinks: any = [];
+  showPurchaseLinksModal = false;
+  isVideoLoading = true;
 
-  constructor(private route:ActivatedRoute,
+  constructor(private route: ActivatedRoute,
     private api: ApiService,
     private state: GlobalStateService,
     private title: Title,
@@ -28,14 +31,14 @@ export class CategoryComponent implements OnInit,OnDestroy {
     this.state.mobileNavToggle.next(false);
 
   }
-  
+
   ngOnInit(): void {
     // does not render when there is a update in params
     // const category = this.route.snapshot.paramMap.get('category');
     // console.log(category);
 
     //renders when there is a update in params
-    this.route.params.pipe(takeWhile(_=>this.alive)).subscribe(routeParams => {
+    this.route.params.pipe(takeWhile(_ => this.alive)).subscribe(routeParams => {
       this.state.mobileNavToggle.next(false);
       this.show404 = false;
       this.loadMetaData(routeParams['category']);
@@ -46,17 +49,23 @@ export class CategoryComponent implements OnInit,OnDestroy {
     //   this.state.mobileNavToggle.next(false);
     //   this.fragment = fragment;
     // })
-    
+
   }
 
-  loadMetaData(category: string){
-    this.api.getSlugData(category).pipe(takeWhile(_=>this.alive)).subscribe({
+  loadMetaData(category: string) {
+    this.api.getSlugData(category).pipe(takeWhile(_ => this.alive)).subscribe({
       next: (res: any) => {
         console.log(res);
-        res.data.cover_media = res.data.cover_media.map((obj: any)=>{
+        res.data.cover_media = res.data.cover_media.map((obj: any) => {
           return {
             ...obj,
             sanatized_url: this.getVideoLink(obj.full_url)
+          }
+        });
+        res.data.category_products = res.data.category_products.map((obj: any, i: number) => {
+          return {
+            ...obj,
+            currentIndex: 0
           }
         })
         this.metaData = res.data;
@@ -76,15 +85,15 @@ export class CategoryComponent implements OnInit,OnDestroy {
     })
   }
 
-  seoTags(data: any){
+  seoTags(data: any) {
     this.title.setTitle(data.title);
     this.meta.updateTag({
       name: 'description',
       content: data.description
     })
-    data.meta.forEach((obj: any)=>{
-      if(obj.attributes){
-        for(let key in obj.attributes){
+    data.meta.forEach((obj: any) => {
+      if (obj.attributes) {
+        for (let key in obj.attributes) {
           this.meta.updateTag({
             property: key,
             content: obj.attributes[key]
@@ -95,17 +104,53 @@ export class CategoryComponent implements OnInit,OnDestroy {
   }
 
   getVideoLink(url: string) {
-    console.log(url);
     url = url.split('?')[0] + '?autoplay=0&mute=0&showinfo=1&controls=1;';
     console.log(url);
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
-  load(){
-    console.log('loaded');
+  buyOptions(url: any) {
+    this.api.getVarientData(url.slice(3)).subscribe({
+      next: (res: any) => {
+        console.log(res?.data?.product_variant_purchase_links);
+        this.purchaseLinks = res?.data?.product_variant_purchase_links;
+        this.showPurchaseLinksModal = true;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
   }
 
-  ngOnDestroy(){
+  getVendorImage(vendor: string) {
+    switch (vendor) {
+      case "amazon":
+        return 'assets/amazon.png';
+      case "flipkart":
+        return 'assets/flipkart.png';
+      // case "tata_cliq":
+      //   return 'assets/tata-cliq.jpg';
+      // case "jio_mart":
+      //   return 'assets/jiomart.png';
+      case "reliance_digital":
+        return 'assets/reliance-digital.png';
+      case "croma":
+        return 'assets/croma.jpg';
+      default:
+        return '';
+    }
+  }
+
+  loadedCount = 0;
+  load() {
+    console.log('loaded');
+    this.loadedCount++;
+    if(this.loadedCount == 2){
+      this.isVideoLoading = false;
+    }
+  }
+
+  ngOnDestroy() {
     this.alive = false;
   }
 
