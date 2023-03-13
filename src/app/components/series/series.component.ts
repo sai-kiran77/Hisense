9d79b5e878d57c84ddd932ec5db4c0ee449aaf93
sai kiran <sai.kiran3916@gmail.com>
2023-03-13 18:13:11 +0530

    slider in home category

--- src/app/components/series/series.component.ts
+++ src/app/components/series/series.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
-// import Swiper core and required modules
-import SwiperCore, { Pagination, Navigation } from "swiper";
+// // import Swiper core and required modules
+// import SwiperCore, { Pagination, Navigation } from "swiper";
 
-// install Swiper modules
-SwiperCore.use([Pagination, Navigation]);
+// // install Swiper modules
+// SwiperCore.use([Pagination, Navigation]);
+
+declare var Swiper: any;
 
 
 @Component({
@@ -15,9 +17,37 @@ SwiperCore.use([Pagination, Navigation]);
 })
 export class SeriesComponent implements OnInit {
 
+  slides = [
+    {
+      click_link: '',
+      desktop_image_full_url: 'assets/about-hisense/overview/hisense-uled-8k-tv-u80g.jpg',
+      mobile_image_full_url: 'assets/about-hisense/overview/hisense-uled-8k-tv-u80g-m.jpg'
+    },
+    {
+      click_link: '',
+      desktop_image_full_url: 'https://global.hisense.com/dam/jcr:3fcfa2b7-ca09-45b9-8075-91942d514ce5/hisense-120l5-ultra-short-throw-laser-cinema.jpg',
+      mobile_image_full_url: 'https://global.hisense.com/dam/jcr:1cb000f7-112e-4643-a541-ef51b17b3dcc/hisense-120l5-ultra-short-throw-laser-cinema-m.jpg'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    let swiper = new Swiper(".laserTvSwiper", {
+      slidesPerView: 1,
+      loop: true,
+      // pagination: {
+      //   el: ".swiper-pagination",
+      //   clickable: true,
+      // },
+      autoplay: {
+        delay: 6000,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
   }
 
 }
