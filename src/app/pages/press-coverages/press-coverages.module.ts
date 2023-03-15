3b0cb7bf7e1c2a2a3538e9cb5dc0861186cf4204
sai kiran <sai.kiran3916@gmail.com>
2023-03-15 18:36:27 +0530

    header link changes

--- src/app/pages/press-coverages/press-coverages.module.ts
+++ src/app/pages/press-coverages/press-coverages.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    PressCoveragesComponent
+    // PressCoveragesComponent
   ],
   imports: [
     CommonModule,
--- src/app/pages/tech-testimonials/tech-testimonials.module.ts
+++ src/app/pages/tech-testimonials/tech-testimonials.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    TechTestimonialsComponent
+    // TechTestimonialsComponent
   ],
   imports: [
     CommonModule,
