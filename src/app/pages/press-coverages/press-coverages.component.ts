1d3a0ba696f45265b9ca16dc797349832c7e7332
sai kiran <sai.kiran3916@gmail.com>
2023-03-15 12:42:49 +0530

    pree coverage page changes

--- src/app/pages/press-coverages/press-coverages.component.ts
+++ src/app/pages/press-coverages/press-coverages.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ApiService } from 'src/app/services/api.service';
 import { GlobalStateService } from 'src/app/services/global-state.service';
+
+declare var Swiper: any;
 
 @Component({
   selector: 'app-press-coverages',
@@ -29,6 +31,7 @@ export class PressCoveragesComponent implements OnInit {
 
   changeTab(tab: any) {
     this.currentTab = tab;
+    this.currentPage = 1;
     if (this.currentTab == 'Global') {
       this.metaData = [];
       this.totalPagesinTen = [];
@@ -44,7 +47,7 @@ export class PressCoveragesComponent implements OnInit {
 
   changeYearTab(tab: any) {
     this.currentSelectedYearTab = tab;
-    
+
   }
 
   constructor(private api: ApiService,
@@ -54,9 +57,38 @@ export class PressCoveragesComponent implements OnInit {
     this.title.setTitle('Hisense India | Press Coverages');
   }
 
+  swiperr: any;
+  slides: any = [];
+
   ngOnInit(): void {
     this.getMetaData();
+    setTimeout(() => {
+      this.swiperr = new Swiper(".mySwiperPress", {
+        slidesPerView: 1,
+        // loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        // autoplay: {
+        //   delay: 6000,
+        // },
+        initialSlide: 0,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+      // this.swiperr.loopDestroy();
+      // this.swiperr.loopCreate();
+    })
   }
+
+  paginationPageCountinFive = 0;
+  currentPage = 1;
+  firstPage = 1;
+  lastPage = 0;
+  metaDataCopyv = [];
 
   getMetaData(page?: any) {
     if (page) {
@@ -66,33 +98,65 @@ export class PressCoveragesComponent implements OnInit {
     this.yearRecordsMap = {};
     this.yearRecordsTabs = [];
     this.currentSelectedYearTab = 0;
-    
     this.api.getPressCoverages(this.params).subscribe((res: any) => {
-      res?.data?.data.forEach((obj:any)=>{
-        if(this.yearRecordsMap[obj.published_year]){
+      res?.data?.data.forEach((obj: any) => {
+        if (this.yearRecordsMap[obj.published_year]) {
           this.yearRecordsMap[obj.published_year].push(obj);
-        }else{
+        } else {
           this.yearRecordsTabs.push(obj.published_year);
           this.yearRecordsMap[obj.published_year] = [obj];
         }
       })
-        // this.totalPagesinTen = new Array(res.data.last_page);
-        // if (this.container) {
-          //   this.container.nativeElement.scrollIntoView({ behavior: "smooth" });
-          // } 
-        if(this.yearRecordsTabs.length){
-          this.currentSelectedYearTab = this.yearRecordsTabs[0];
-        }
-        this.metaData = res.data.data.sort((a: any, b: any) => {
-          const dateA: any = new Date(a.published_at);
-          const dateB: any = new Date(b.published_at);
-          return dateB - dateA;
-        });;
-      console.log(this.metaData);
-      console.log(this.yearRecordsMap, this.yearRecordsTabs )
+      // this.totalPagesinTen = new Array(res.data.last_page);
+      // if (this.container) {
+      //   this.container.nativeElement.scrollIntoView({ behavior: "smooth" });
+      // } 
+      if (this.yearRecordsTabs.length) {
+        this.currentSelectedYearTab = this.yearRecordsTabs[0];
+      }
+      this.metaData = res.data.data.sort((a: any, b: any) => {
+        const dateA: any = new Date(a.published_at);
+        const dateB: any = new Date(b.published_at);
+        return dateB - dateA;
+      });
+      this.slides = this.metaData.slice(0, 4);
+      this.swiperr.slideTo(0);
+      this.metaDataCopyv = this.metaData.slice(4);
+      const decimal = this.metaDataCopyv.length / 6;
+      this.paginationPageCountinFive = Math.ceil(decimal);
+      this.lastPage = this.paginationPageCountinFive;
+      this.metaData = this.metaDataCopyv.slice(((this.currentPage - 1) * 6), (this.currentPage * 6));
     }, (err) => {
       console.log(err);
     })
+  }
+
+  nextPage() {
+    if (this.currentPage < this.lastPage) {
+      this.currentPage = this.currentPage + 1;
+      this.metaData = this.metaDataCopyv.slice(((this.currentPage - 1) * 6), (this.currentPage * 6));
+      if (this.container) {
+        this.container.nativeElement.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage = this.currentPage - 1;
+      this.metaData = this.metaDataCopyv.slice(((this.currentPage - 1) * 6), (this.currentPage * 6));
+      if (this.container) {
+        this.container.nativeElement.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }
+
+  jumpToPage(pageNumber: any) {
+    this.currentPage = pageNumber;
+    this.metaData = this.metaDataCopyv.slice(((this.currentPage - 1) * 6), (this.currentPage * 6));
+    if (this.container) {
+      this.container.nativeElement.scrollIntoView({ behavior: "smooth" });
+    }
   }
 
 }
