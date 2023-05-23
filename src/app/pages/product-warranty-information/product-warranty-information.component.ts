a36535181f39ea4f1b42cfca46c17ffdf0d366ca
sai kiran <sai.kiran3916@gmail.com>
2023-05-23 22:53:21 +0530

    warranty info changes

--- src/app/pages/product-warranty-information/product-warranty-information.component.ts
+++ src/app/pages/product-warranty-information/product-warranty-information.component.ts
@@ -9,10 +9,52 @@ import { GlobalStateService } from 'src/app/services/global-state.service';
 })
 export class ProductWarrantyInformationComponent implements OnInit {
 
-  product_purchase_date = '';
+  minYear = new Date().getFullYear() + 1;
+  minYearCopy = this.minYear;
+  maxYear = 1970;
+  maxYearCopy = this.maxYear;
 
-  setRegistrationDate(e: any){
+  monthMap = {
+    '01': "January",
+    '02': "February",
+    '03': "March",
+    '04': "April",
+    '05': "May",
+    '06': "June",
+    '07': "July",
+    '08': "August",
+    '09': "September",
+    '10': "October",
+    '11': "November",
+    '12': "December",
+  }
 
+  daysInMonthMap: any = {
+    '01': "31",
+    '02': "28",
+    '03': "31",
+    '04': "30",
+    '05': "31",
+    '06': "30",
+    '07': "31",
+    '08': "31",
+    '09': "30",
+    '10': "31",
+    '11': "30",
+    '12': "31",
+  }
+  dateSelected: any;
+  changeDate(e: any){
+    console.log(e.target.value);
+    // 2023-05-10
+    this.warranty = null;
+    this.dateSelected = e.target.value;
+    const [year,month,date] = e.target.value.split('-');
+    this.updateYear(year);
+    if(this.selectedYear){
+      this.updateMonth(month);
+    }
+    
   }
 
   metaData: any = [];
@@ -20,7 +62,14 @@ export class ProductWarrantyInformationComponent implements OnInit {
   selectedCat: any = null;
   productVarients: any = [];
   selectedProd: any = null;
+  selectedProdId: any = null;
   updateCategory(event: any) {
+    this.warranty = null;
+    this.selectedProd = null;
+    this.selectedProdId = null;
+    this.dateSelected = null;
+    this.minYear = this.minYearCopy;
+    this.maxYear = this.maxYearCopy;
     this.productVarients = [];
     this.metaData.find((obj: any)=>obj.id == event.target.value)?.products.forEach((object: any)=>{
       object.product_variants.forEach((o: any)=>{
@@ -30,17 +79,68 @@ export class ProductWarrantyInformationComponent implements OnInit {
     console.log(this.productVarients);
   }
 
+  minYearObj: any;
+  maxYearObj: any;
+
+  minYearMinMonth: any = 12;
+  maxYearMaxMonth: any = 1;
+
   updateProduct(event: any) {
+    this.warranty = null;
+    this.dateSelected = null;
+    this.minYear = this.minYearCopy;
+    this.maxYear = this.maxYearCopy;
     this.selectedProd = this.productVarients.find((obj: any)=>obj.id == event.target.value);
+    console.log(this.selectedProd);
+    this.selectedProd.years_of_purchase.forEach((obj: any)=>{
+      if(obj.year < this.minYear){
+        this.minYear = obj.year;
+        this.minYearObj = obj;
+      }
+      
+      if(obj.year > this.maxYear){
+        this.maxYear = obj.year;
+        this.maxYearObj = obj;
+      }
+    })
+
+    this.minYearObj.months_of_purchase.forEach((obj: any)=>{
+      if(obj.month < this.minYearMinMonth){
+        this.minYearMinMonth = String(obj.month).padStart(2, '0')
+      }
+    })
+
+    this.maxYearObj.months_of_purchase.forEach((obj: any)=>{
+      if(obj.month > this.maxYearMaxMonth){
+        this.maxYearMaxMonth = String(obj.month).padStart(2, '0')
+      }
+    })
   }
 
   selectedYear: any;
-  updateYear(event: any) {
-    this.selectedYear = this.selectedProd.years_of_purchase.find((obj: any)=>obj.year == event.target.value);
+  updateYear(value: any) {
+    this.selectedYear = this.selectedProd.years_of_purchase.find((obj: any)=>obj.year == value);
   }
   warranty: any;
-  updateMonth(event: any) {
-    this.warranty = this.selectedYear.months_of_purchase.find((obj: any)=>obj.month == event.target.value);
+  updateMonth(value: any) {
+    const selectedMonthInfo = this.selectedYear.months_of_purchase.find((obj: any)=>obj.month == value);
+    if(selectedMonthInfo){
+      if(selectedMonthInfo?.date_ranges.length){
+        const formatedDate = this.dateSelected.replace(/-/g, "");
+        for(let i = 0;i<selectedMonthInfo.date_ranges.length;i++){
+          const obj = selectedMonthInfo.date_ranges[i];
+          if(formatedDate <= obj.end_date && formatedDate >= obj.start_date){
+            this.warranty = obj;
+            break;
+          }
+        }
+        if(!this.warranty){
+          this.warranty = selectedMonthInfo.default;
+        }
+      }else{
+        this.warranty = selectedMonthInfo.default;
+      }
+    }
     console.log(this.warranty);
   }
 
